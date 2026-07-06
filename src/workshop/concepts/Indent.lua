097925a2bc0f36588c847060d61b34dafe3506a7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-06 23:07:57 +0200

    [~] Sync with workshop.

--- src/workshop/concepts/Indent.lua
+++ src/workshop/concepts/Indent.lua
@@ -2,32 +2,49 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-11
+  Last mod.: 2026-07-05
 ]]
 
 -- Imports:
 local create_instance = request('!.table.create_instance')
-local RangePointClass = request('!.concepts.RangePoint')
+local RangePoint = request('!.concepts.RangePoint')
 
-local Core
+local RangePoint = RangePoint.create()
+RangePoint:SetMinValue(0)
+RangePoint:SetMaxValue(60)
+RangePoint:SetValue(RangePoint:GetMinValue())
 
-local RangePoint = RangePointClass.create()
-RangePoint.min_value = 0
-RangePoint.max_value = 60
-RangePoint.value = 0
+--[[
+  Data storage format
 
-Core =
+    1 [s] Indent chunk
+    2 [t] Range point of current indent level
+]]
+local Core =
   {
-    indent_chunk = '  ',
-    RangePoint = RangePoint,
+    '  ',
+    RangePoint,
   }
 
 local Interface
 Interface =
   {
+    GetIndentChunk = function(Me) return Me[1] end,
+    SetIndentChunk =
+      function(Me, str)
+        assert_string(str)
+
+        Me[1] = str
+      end,
+
+    GetRangePoint = function(Me) return Me[2] end,
+
     ToString =
       function(Me)
-        return string.rep(Me.indent_chunk, Me:GetRangePoint():GetValue())
+        local indent_chunk = Me:GetIndentChunk()
+        local indent_level = Me:GetRangePoint():GetValue()
+
+        return string.rep(indent_chunk, indent_level)
       end,
 
     Inc =
@@ -43,11 +60,6 @@ Interface =
       function(OptCore)
         return create_instance(OptCore or Core, Interface)
       end,
-
-    GetRangePoint =
-      function(Me)
-        return Me.RangePoint
-      end,
   }
 
 -- Export:
@@ -55,4 +67,5 @@ return Interface
 
 --[[
   2026-05-11
+  2026-07-05
 ]]
