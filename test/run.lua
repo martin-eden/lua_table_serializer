8f8b08753489474da5bc6b95939d9f789d32094e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-21 21:08:42 +0200

    [.^] [test] Code revisit.

--- test/run.lua
+++ test/run.lua
@@ -2,22 +2,46 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-06-20
+  Last mod.: 2026-08-30
 ]]
 
 package.path = package.path .. ';../deploy/?.lua'
-local t2s = require('serialize_lua_graph')
 
-local test =
-  function(val)
-    print(t2s(val))
+-- Print graph as Lua code
+local test
+do
+  local g2s = require('serialize_lua_graph')
+  local Options =
+    {
+      -- style = 'minimal',
+      style = 'readable_short',
+      -- style = 'readable_long',
+      -- use_compact_indices = not true,
+      -- use_compact_sequences = true,
+      -- omit_tail_delimiter = not true,
+    }
+  test =
+    function(Graph)
+      io.stdout:write(g2s(Graph, Options))
+      io.stdout:write('\n')
+    end
+end
+
+-- Print graph with self-links in keys and values
+do
+  local Graph
+
+  do
+    Graph = { }
+    Graph[{ Graph }] = { Graph }
   end
 
-local Graph = { }
-Graph[{ Graph }] = { Graph }
-test(Graph)
+  -- Graph = _G
+
+  test(Graph)
+end
 
 --[[
-  2026-06-17
-  2026-06-20
+  2026 # # #
+  2026-08-30
 ]]
